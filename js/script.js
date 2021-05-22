80152b8234c9add674d6f78f724c2be6b763f61b
Heidi Fryzell <hfryzell@gmail.com>
2021-05-22 11:53:08 -0400

    indvidual repos displaying under header as h3s

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 // div where profile will appear
 const profileDiv = document.querySelector(".overview");
 
+// ul where repos are displayed
+const displayRepos = document.querySelector(".repo-list");
+
 // my GitHub username
 const username = "heidi37";
 
 //fetch info from my GitHub profile
 const getProfileInfo = async function () {
     const response = await fetch (`https://api.github.com/users/${username}`);
-    const data = await response.json();
-    console.log(data);
-    displayUserInfo(data);
+    const profileData = await response.json();
+    // console.log(profileData);
+    displayUserInfo(profileData);
 };
 
 getProfileInfo();
@@ -30,6 +33,29 @@ const displayUserInfo = function (data) {
     </div> 
     `;
     profileDiv.append(userInfoDiv);
+    getRepos();
 };
+
+//function to fetch the user's repots
+const getRepos = async function () {
+    const response = await fetch (`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`);
+    const repoData = await response.json();
+    console.log(repoData);
+    displayRepoDetails(repoData);
+}
+
+
+
+//function to display info about individual repo
+const displayRepoDetails = function (repos) {
+    for (const repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        displayRepos.append(li);
+    };
+}
+
+
 
 
